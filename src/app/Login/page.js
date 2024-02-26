88152249b1d7bde6e4e365e03d8fd082d688e4c6
ManishKuPatra254 <manishpatra254@gmail.com>
2024-02-26 18:09:17 +0530

    done with the ui and login service

--- src/app/Login/page.js
+++ src/app/Login/page.js
@@ -1,26 +1,80 @@
-import React, { Fragment } from 'react'
-import './page.css'
+"use client"
+import React, { Fragment, useState } from 'react'
+import './Login.css'
 import Link from 'next/link'
 import { Navbar } from '../Navbar/Navbar'
+import Alert from '@mui/material/Alert';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 function Login() {
+
+    const [alertMessage, setAlertMessage] = useState('');
+    const [alertSeverity, setAlertSeverity] = useState('');
+
+
+    const showAlert = (message, severity) => {
+        setAlertMessage(message);
+        setAlertSeverity(severity);
+    };
+
+    const handleAlertClose = () => {
+        setAlertMessage('');
+        setAlertSeverity('');
+    };
+
+    const handleTogglePasswordVisibility = () => {
+        setFormData((prevState) => ({
+            ...prevState,
+            showPassword: !prevState.showPassword,
+        }));
+    };
     return (
         <Fragment>
             <Navbar />
-            <form style={{ width: "40%", margin: "auto", marginTop: "80px" }}>
-                <h2>Login to your account</h2>
-                <div className="mb-3">
-                    <label for="exampleInputEmail1" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                    <div id="emailHelp" className="form-text">Well never share your email with anyone else.</div>
-                </div>
-                <div className="mb-3">
-                    <label for="exampleInputPassword1" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" />
-                </div>
-                <button type="submit" style={{ width: "100%", background: "#24AB70", border: "none", borderRadius: "8px", padding: "9px", color: "white", marginTop: "15px" }} >Login now</button>
-                <p style={{ display: "flex", justifyContent: "center", marginTop: "25px" }}>Dont have a account <Link href={'/Signup'}>Signup</Link></p>
-            </form>
+            <div className="main_login_form">
+                <form>
+                    {alertMessage && (
+                        <Alert sx={{ mb: 2 }} severity={alertSeverity} onClose={handleAlertClose}>
+                            {alertMessage}
+                        </Alert>
+                    )}
+                    <h2>Login to your account</h2>
+                    <div className="mb-3">
+                        <label for="exampleInputEmail1" className="form-label">Email address</label>
+                        <input type="email"
+                            name='email'
+                            className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                        <div id="emailHelp" className="form-text">Well never share your email with anyone else.</div>
+                    </div>
+                    <div className="mb-3 position-relative">
+                        <label htmlFor="exampleInputPassword1" className="form-label">
+                            Password
+                        </label>
+                        <div className="input-group">
+                            <input
+                                // type={formData.showPassword ? 'text' : 'password'}
+                                name='password'
+                                // value={formData.password}
+                                // onChange={handleInputChange}
+                                className="form-control"
+                                id="exampleInputPassword1"
+                            />
+                            {/* <div className="input-group-append">
+                                <button
+                                    className="btn btn-dark"
+                                    type='button'
+                                    onClick={handleTogglePasswordVisibility}
+                                >
+                                    {formData.showPassword ? <VisibilityOff /> : <Visibility />}
+                                </button>
+                            </div> */}
+                        </div>
+                    </div>
+                    <button type="submit" style={{ width: "100%", background: "#24AB70", border: "none", borderRadius: "8px", padding: "9px", color: "white", marginTop: "15px" }} >Login now</button>
+                    <p style={{ display: "flex", justifyContent: "center", marginTop: "25px" }}>Dont have a account <Link href={'/Signup'}>Signup</Link></p>
+                </form>
+            </div>
         </Fragment>
     )
 }
--- src/app/Signup/page.jsx
+++ src/app/Signup/page.jsx
@@ -2,8 +2,13 @@
 
 import React, { Fragment, useState } from 'react'
 import { Navbar } from '../Navbar/Navbar'
+import './Signup.css'
 import Link from 'next/link'
 import { SignupUser } from '../Service/auth.service'
+import Alert from '@mui/material/Alert';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
 
 function Signup() {
 
@@ -13,17 +18,21 @@ function Signup() {
         email: "",
         password: "",
     })
-
+    const [alertMessage, setAlertMessage] = useState('');
+    const [alertSeverity, setAlertSeverity] = useState('');
 
     const handleSignupUser = async (e) => {
         e.preventDefault();
-        try {
-            const response = await SignupUser(formData)
-            console.log(response);
-            alert(response.msg);
+        if (!formData.first_name || !formData.last_name || !formData.email || !formData.password) {
+            showAlert('Please fill in all required fields', 'error');
+            return;
         }
-        catch (error) {
-            alert(error.msg)
+        try {
+            const response = await SignupUser(formData);
+            console.log(response);
+            showAlert(response.msg, 'success');
+        } catch (error) {
+            showAlert(error.msg, 'error');
         }
     }
 
@@ -35,48 +44,90 @@ function Signup() {
             [name]: value,
         }));
     };
+
+    const showAlert = (message, severity) => {
+        setAlertMessage(message);
+        setAlertSeverity(severity);
+    };
+
+    const handleAlertClose = () => {
+        setAlertMessage('');
+        setAlertSeverity('');
+    };
+
+    const handleTogglePasswordVisibility = () => {
+        setFormData((prevState) => ({
+            ...prevState,
+            showPassword: !prevState.showPassword,
+        }));
+    };
+
     return (
         <Fragment>
             <Navbar />
-            <form style={{ width: "40%", margin: "auto", marginTop: "80px" }} onSubmit={handleSignupUser}>
-                <h2>Signup to your account</h2>
-                <div className="mb-3">
-                    <label for="exampleInputEmail1" className="form-label">First name</label>
-                    <input type="text"
-                        name='first_name'
-                        value={formData.first_name}
-                        onChange={handleInputChange}
-                        className="form-control"
-                        id="exampleInputEmail1" aria-describedby="emailHelp" />
-                </div>
-                <div className="mb-3">
-                    <label for="exampleInputEmail1" className="form-label">Last name</label>
-                    <input type="text"
-                        name='last_name'
-                        value={formData.last_name}
-                        onChange={handleInputChange}
-                        className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                </div>
-                <div className="mb-3">
-                    <label for="exampleInputEmail1" className="form-label">Email address</label>
-                    <input type="email"
-                        name='email'
-                        value={formData.email}
-                        onChange={handleInputChange}
-                        className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                    <div id="emailHelp" className="form-text">Well never share your email with anyone else.</div>
-                </div>
-                <div className="mb-3">
-                    <label for="exampleInputPassword1" className="form-label">Password</label>
-                    <input type="password"
-                        name='password'
-                        value={formData.password}
-                        onChange={handleInputChange}
-                        className="form-control" id="exampleInputPassword1" />
-                </div>
-                <button type="submit" style={{ width: "100%", background: "#24AB70", border: "none", borderRadius: "8px", padding: "9px", color: "white", marginTop: "15px" }} >Signup now</button>
-                <p style={{ display: "flex", justifyContent: "center", marginTop: "25px" }}>Dont have a account <Link href={'/Login'}>Login</Link></p>
-            </form>
+            <div className="main_signup_form">
+                <form onSubmit={handleSignupUser}>
+                    {alertMessage && (
+                        <Alert sx={{ mb: 2 }} severity={alertSeverity} onClose={handleAlertClose}>
+                            {alertMessage}
+                        </Alert>
+                    )}
+
+                    <h2>Create an account</h2>
+                    <div className="mb-3">
+                        <label for="exampleInputEmail1" className="form-label">First name</label>
+                        <input type="text"
+                            name='first_name'
+                            value={formData.first_name}
+                            onChange={handleInputChange}
+                            className="form-control"
+                            id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    </div>
+                    <div className="mb-3">
+                        <label for="exampleInputEmail1" className="form-label">Last name</label>
+                        <input type="text"
+                            name='last_name'
+                            value={formData.last_name}
+                            onChange={handleInputChange}
+                            className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    </div>
+                    <div className="mb-3">
+                        <label for="exampleInputEmail1" className="form-label">Email address</label>
+                        <input type="email"
+                            name='email'
+                            value={formData.email}
+                            onChange={handleInputChange}
+                            className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                        <div id="emailHelp" className="form-text">Well never share your email with anyone else.</div>
+                    </div>
+                    <div className="mb-3 position-relative">
+                        <label htmlFor="exampleInputPassword1" className="form-label">
+                            Password
+                        </label>
+                        <div className="input-group">
+                            <input
+                                type={formData.showPassword ? 'text' : 'password'}
+                                name='password'
+                                value={formData.password}
+                                onChange={handleInputChange}
+                                className="form-control"
+                                id="exampleInputPassword1"
+                            />
+                            <div className="input-group-append">
+                                <button
+                                    className="btn btn"
+                                    type='button'
+                                    onClick={handleTogglePasswordVisibility}
+                                >
+                                    {formData.showPassword ? <VisibilityOff /> : <Visibility />}
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <button type="submit" style={{ width: "100%", background: "#24AB70", border: "none", borderRadius: "8px", padding: "9px", color: "white", marginTop: "15px" }} >Create account</button>
+                    <p style={{ display: "flex", justifyContent: "center", marginTop: "25px" }}>Dont have a account <Link href={'/Login'}>Login</Link></p>
+                </form>
+            </div>
         </Fragment>
     )
 }
